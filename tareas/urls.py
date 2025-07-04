309e08f72ac27d050703361389eccb9a48fdcc34
Luis Miraglio <lmiraglio@conectar.red>
2025-07-04 18:49:03 -0300

    Cambios varios

--- tareas/urls.py
+++ tareas/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from tareas.views import lista_tareas, crear_tarea
 from .views import eliminar_tarea
+from .views import lista_tareas, crear_tarea, eliminar_tarea, cambiar_estado
+
+
 
 urlpatterns = [
     path('', lista_tareas, name='lista_tareas'),  # /tareas/
     path('nueva/', crear_tarea, name='crear_tarea'),  # /tareas/nueva/
     path('eliminar/<int:tarea_id>/', eliminar_tarea, name='eliminar_tarea'),
+    path('cambiar_estado/<int:tarea_id>/', cambiar_estado, name='cambiar_estado'),
+
+
 ]
 
--- tareas/views.py
+++ tareas/views.py
@@ -21,3 +21,10 @@ def eliminar_tarea(request, tarea_id):
     tarea = get_object_or_404(Tarea, id=tarea_id)
     tarea.delete()
     return redirect('lista_tareas')
+
+def cambiar_estado(request, tarea_id):
+    tarea = get_object_or_404(Tarea, id=tarea_id)
+    tarea.completada = not tarea.completada
+    tarea.save()
+    return redirect('lista_tareas')
+
